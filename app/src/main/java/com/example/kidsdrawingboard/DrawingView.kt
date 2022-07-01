6b47575c5bd91b16054f704cc3109c77f3d4dd65
GKS02 <gurkaran.ug20@nsut.ac.in>
2022-07-01 21:09:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kidsdrawingboard/DrawingView.kt
@@ -0,0 +1,139 @@
+package com.example.kidsdrawingboard
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.MotionEvent
+import android.graphics.*
+import android.view.View
+
+class DrawingView(context:Context,attrs:AttributeSet) : View(context,attrs) {
+    private var mDrawPath: CustomPath? =  null
+    private var mCanvasBitmap: Bitmap? = null
+
+    private var mDrawPaint: Paint? =  null // The Paint class holds the style and color information about how to draw geometries, text and bitmaps.
+    private var mCanvasPaint: Paint? = null // Instance of canvas paint view.
+
+    private var mBrushSize: Float = 0.toFloat() // A variable for stroke/brush size to draw on the canvas.
+
+     private var color = Color.BLACK
+
+    private var canvas: Canvas? = null
+
+    private val mPaths = ArrayList<CustomPath>()
+
+    private val mUndoPaths = ArrayList<CustomPath>()
+
+    init {
+        setUpDrawing()
+    }
+
+
+    private fun setUpDrawing() {
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+
+        mDrawPaint?.color = color
+
+        mDrawPaint?.style = Paint.Style.STROKE // This is to draw a STROKE style
+        mDrawPaint?.strokeJoin = Paint.Join.ROUND // This is for store join
+        mDrawPaint?.strokeCap = Paint.Cap.ROUND // This is for stroke Cap
+
+        mCanvasPaint = Paint(Paint.DITHER_FLAG) // Paint flag that enables dithering when blitting.
+
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, wprev: Int, hprev: Int) {
+        super.onSizeChanged(w, h, wprev, hprev)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+
+     override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        mCanvasBitmap?.let {
+            canvas.drawBitmap(it, 0f,   0f, mCanvasPaint)
+        }
+
+
+        for (p in mPaths) {
+            mDrawPaint?.strokeWidth = p.brushThickness
+            mDrawPaint?.color = p.color
+            canvas.drawPath(p, mDrawPaint!!)
+        }
+
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val touchX = event.x
+        val touchY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+
+                mDrawPath!!.reset()
+                mDrawPath!!.moveTo(
+                    touchX,
+                    touchY
+                )
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                mDrawPath!!.lineTo(
+                    touchX,
+                    touchY
+                )
+            }
+
+            MotionEvent.ACTION_UP -> {
+
+                mPaths.add(mDrawPath!!)
+
+                mDrawPath = CustomPath(color, mBrushSize)
+            }
+            else -> return false
+        }
+
+        invalidate()
+        return true
+    }
+
+
+    fun setSizeForBrush(newSize: Float) {
+        mBrushSize = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, newSize,
+            resources.displayMetrics
+        )
+        mDrawPaint!!.strokeWidth = mBrushSize
+    }
+
+
+    fun setColor(newColor: String) {
+        color = Color.parseColor(newColor)
+        mDrawPaint!!.color = color
+    }
+
+
+    fun onClickUndo() {
+        if (mPaths.size > 0) {
+
+            mUndoPaths.add(mPaths.removeAt(mPaths.size - 1))
+            invalidate() // Invalidate the whole view. If the view is visible
+        }
+    }
+    internal inner class CustomPath(var color:Int,var brushThickness:Float):Path()
+}
